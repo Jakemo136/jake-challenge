3d74667d0ad73ef7abda91eebcd0cc61d7037c5c
Jake Mosher <jake.mosher@simpleenergy.com>
2021-07-19 14:58:15 -0600

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const DemoContainer = () => {
+
+  return(
+    <div className="demo-list__container">
+      Hello!
+    </div>
+  )
+}
+
+export default DemoContainer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DemoContainer from './components/index';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DemoContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
